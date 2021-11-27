7ff45df993f711c9da330d60f8caa3775b951b36
Pouillard <antoinepouillard@hotmail.fr>
2021-11-27 21:22:31 +0100

    ajout supp

--- controllers/product.js
+++ controllers/product.js
@@ -1,7 +1,8 @@
+const product = require('../models/product');
 const Product = require('../models/product');
 
 exports.createProduct = (req, res) => {
-    const prod = new Product(req.body)
+    const prod = new Product(req.body);
 
     prod.save()
         .then((product) => {
@@ -15,15 +16,56 @@ exports.createProduct = (req, res) => {
 
 exports.getOneProduct = (req, res) => {
     const id = req.params.id;
-    const prod = new Product();
-    console.log(Product);
-
-    Product.findOne({ _id: id })()
-        .then((product) => {
+    Product.findOne({ _id: id }, (err, product) => {
+        if (err) {
+            return res.status(400).json({ err });
+        } else {
             return res.status(200).json({ product });
-        })
-        .catch((error) => {
-            return res.status(400).json({ error })
-        });
+        }
+    })
+};
 
-}
+exports.getAllProducts = (req, res) => {
+    Product.find()
+        .then((products) => { return res.status(200).json({ products }) })
+        .catch((error) => { return res.status(200).json({ error }) });
+};
+
+exports.update = (req, res) => {
+    const id = req.params.id;
+    Product.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update Product with id=${id}. Maybe Product was not found!`
+                });
+            } else res.send({ message: "Product was updated successfully." });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Product with id=" + id
+            });
+        });
+};
+
+exports.delete = (req, res) => {
+    const id = req.params.id;
+
+    Product.findByIdAndRemove(id)
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot delete Product with id=${id}. Maybe Producg was not found!`
+                });
+            } else {
+                res.send({
+                    message: "Product was deleted successfully!"
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Product with id=" + id
+            });
+        });
+};
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const ProdCtrl = require('../controllers/product');
 
-router.put('/', ProdCtrl.createProduct); // Creer dans la base
+router.post('/', ProdCtrl.createProduct); // Creer dans la base
+
 router.get('/:id', ProdCtrl.getOneProduct); // AFFICHE PAR ID 
+
+router.get('/', ProdCtrl.getAllProducts); // AFFICHE TOUS LES PRODUCTS 
+
+router.put("/:id", ProdCtrl.update); // UPDATE A PRODUCT AVEC ID
+
+router.delete("/:id", ProdCtrl.delete); // DELETE A PRODUCT AVEC ID
 
 module.exports = router;
